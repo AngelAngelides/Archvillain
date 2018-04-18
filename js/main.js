265fc4da569a78e8f86ca8136f0b20bcf6847e0a
Angel Angelides <angelangelides@gmail.com>
2018-04-18 19:01:51 +0200

    added swiper functionality

--- js/main.js
+++ js/main.js
@@ -1,52 +1,23 @@
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-var slideIndex = 0;
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
 
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: true
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
 
-carousel();
-showDivs(slideIndex);
-
-function plusSlides(n) {
-  showDivs(slideIndex += n);
-}
-
-
-function currentSlide(n) {
-  showDivs(slideIndex = n);
-}
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(carousel, 10000); // Change image every 10 seconds
-}
-
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-   x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-  x[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
