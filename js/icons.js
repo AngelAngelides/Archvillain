a24c2aa5c05e61fe686a9f65e24a96ba61673ca6
Angel Angelides <angelangelides@gmail.com>
2018-04-18 15:32:06 +0200

    please work disappearing icon :-(

--- js/icons.js
+++ js/icons.js
@@ -2,7 +2,7 @@ window.onpageshow =
 function(event){
 if (event.persisted){
 //for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
-document.getElementById('div1').innerHTML = '<a href="dosomething"><img src="images/logo/fb1" onmouseover="this.src="images/logo/fb2";" onmouseout="this.src="images/logo/fb1";"></a>';
+document.getElementById('header-social').innerHTML = '<a href="dosomething"><img src="images/logo/fb1" onmouseover="this.src="images/logo/fb2";" onmouseout="this.src="images/logo/fb1";"></a>';
 }
 }
 
@@ -10,7 +10,7 @@ window.onpageshow =
 function(event){
 if (event.persisted){
 //for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
-document.getElementById('div1').innerHTML = '<a href="dosomething"><img src="images/logo/in1" onmouseover="this.src="images/logo/in2";" onmouseout="this.src="images/logo/in1";"></a>';
+document.getElementById('header-social').innerHTML = '<a href="dosomething"><img src="images/logo/in1" onmouseover="this.src="images/logo/in2";" onmouseout="this.src="images/logo/in1";"></a>';
 }
 }
 
@@ -18,6 +18,30 @@ window.onpageshow =
 function(event){
 if (event.persisted){
 //for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
-document.getElementById('div1').innerHTML = '<a href="dosomething"><img src="images/logo/tw1" onmouseover="this.src="images/logo/tw2";" onmouseout="this.src="images/logo/tw1";"></a>';
+document.getElementById('header-social').innerHTML = '<a href="dosomething"><img src="images/logo/tw1" onmouseover="this.src="images/logo/tw2";" onmouseout="this.src="images/logo/tw1";"></a>';
+}
+}
+
+window.onpageshow = 
+function(event){
+if (event.persisted){
+//for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
+document.getElementById('footer-social').innerHTML = '<a href="dosomething"><img src="images/logo/fb1" onmouseover="this.src="images/logo/fb2";" onmouseout="this.src="images/logo/fb1";"></a>';
+}
+}
+
+window.onpageshow = 
+function(event){
+if (event.persisted){
+//for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
+document.getElementById('footer-social').innerHTML = '<a href="dosomething"><img src="images/logo/in1" onmouseover="this.src="images/logo/in2";" onmouseout="this.src="images/logo/in1";"></a>';
+}
+}
+
+window.onpageshow = 
+function(event){
+if (event.persisted){
+//for every div and image you want changed back you can add a loop here to change all at once or just one div by name//
+document.getElementById('footer-social').innerHTML = '<a href="dosomething"><img src="images/logo/tw1" onmouseover="this.src="images/logo/tw2";" onmouseout="this.src="images/logo/tw1";"></a>';
 }
 }
