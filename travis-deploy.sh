a2025823b02760e8fc3eda818d6bfcfe35f472de
Timo Welde <tjwelde@gmail.com>
2018-04-20 19:19:15 +0200

    Differentiate between master & staging

---
+++ travis-deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    FIREBASE_PROJECT="$FIREBASE_MASTER"
+else
+    FIREBASE_PROJECT="$FIREBASE_STAGING"
+fi
+
+firebase --project $FIREBASE_PROJECT--token $FIREBASE_TOKEN deploy
