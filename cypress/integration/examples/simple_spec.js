cf757916945ef0001432837c54ef03bec859bf21
Angelos Angelides <angelangelides@gmail.com>
2019-03-22 16:12:08 +0100

    remove cypress

--- cypress/integration/examples/simple_spec.js
+++
@@ -1,7 +0,0 @@
-describe('My First Test', function() {
-    it('Visits the Kitchen Sink', function() {
-      cy.visit('https://archvillaingames.com')
-
-      cy.get('.newsbox').click()
-    })
-  })
